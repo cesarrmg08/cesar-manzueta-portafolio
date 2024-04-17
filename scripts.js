4ba8c916f1f1a805d30aa20e9adcc1a841b8a7ed
Cesar-Manzueta <cmanzueta@flai.com.do>
2024-04-17 14:05:52 -0400

    Adjusted to manage 2 styles.css files

--- scripts.js
+++ scripts.js
@@ -37,4 +37,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Actualizar el enlace activo también en scroll normal
     window.addEventListener('scroll', updateActiveLink);
+
+    const hamburger = document.querySelector('.hamburger');
+    const navUL = document.querySelector('.navbar ul');
+
+    hamburger.addEventListener('click', function() {
+        navUL.classList.toggle('open');
+    });
+
+    // Escuchar clics en todo el documento
+    document.addEventListener('click', function(event) {
+        // Si el clic NO es en el hamburger y no es dentro del navbar ul
+        if (!hamburger.contains(event.target) && !navUL.contains(event.target)) {
+            navUL.classList.remove('open');  // Cierra el menú
+        }
+    });
+
+    function initSwiper() {
+        new Swiper('.mySwiper', {
+            slidesPerView: 1, // Para móviles, muestra una tarjeta a la vez
+            spaceBetween: 10, // Espacio entre slides para móviles
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 3, // Para pantallas más grandes, muestra 3 tarjetas
+                    spaceBetween: 30, // Espacio entre slides para pantallas más grandes
+                }
+            },
+        });
+    }
+
+    initSwiper(); // Inicia Swiper al cargar
+    window.addEventListener('resize', initSwiper); // Re-inicializar Swiper en cambio de tamaño
 });
+
